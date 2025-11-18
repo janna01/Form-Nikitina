8a2a5f79df0744a60c96374aa05e6a4a23c6e4ba
Janna K <qwerty1023.jk@gmail.com>
2025-11-18 19:45:11 +0500

    Nikitina

---
+++ form.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("contactForm");
+
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const message = document.getElementById("message").value;
+
+        const TELEGRAM_BOT_TOKEN = "7990014475:AAE_GImGFDvY-z_XA1JY99t9h1bknDgK0iY";
+        const TELEGRAM_CHAT_ID = "-4801540981";
+
+        const text = `Сообщение полученное с сайта:
+Имя: ${name}
+Почта: ${email}
+Сообщение: ${message}`;
+
+        try {
+            const response = await fetch(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    chat_id: TELEGRAM_CHAT_ID,
+                    text: text,
+                }),
+            });
+
+            const result = await response.json();
+            console.log("Ответ Telegram:", result);
+
+            if (result.ok) {
+                alert("Ваше сообщение отправлено!");
+                form.reset();
+            } else {
+                alert("Ошибка при отправке!\n\n" + JSON.stringify(result, null, 2));
+            }
+        } catch (err) {
+            console.error("Ошибка соединения:", err);
+            alert("Ошибка соединения!\n\n" + err.message);
+        }
+    });
+});
